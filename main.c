b1557f33cc0cb9e0a6093c82d5d4564c0694fcab
GrischuG <83284905+GrischuG@users.noreply.github.com>
2022-04-08 21:03:57 +0200

    Some more stuff
    Linked List and according functions;
    some more stuff
    NOT YET TESTED!!

--- main.c
+++ main.c
@@ -5,23 +5,99 @@
 
 
 
-#define N 3
+#define N 3 // # of apprentices
+#define MAX_A 20  // max # of apprentices
+#define MAX_BREAD 100 // max # of breads to be made by the A.
+int nbOfA = N;
 
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;  
+pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 
 int flour = INT_MAX;
-int oil;
-int bp; //baking powder
+int oil = INT_MAX;
+int bp = INT_MAX; //baking powder
+
+int breads = 0;
+
+
+/* NODE STRUCTURE */
+struct Node {
+
+    int apprNb; //apprentice
+    
+    struct Node *next;
+    struct Node *prev; 
+
+};
+
+
+/* NEW NODE FUNCTION */
+struct Node* newNode(int i) {
+
+    // Allocate Memory
+    struct Node *node = (struct Node *) malloc(sizeof(struct Node));
+
+    // Init data
+    node->apprNb = i;
+    node->next = NULL;
+    node->prev = NULL; 
+
+    return node;
+}
+
+
+struct Node *linkedList; // Kinda the Current node
+
+/* CREATING NEW APPRENTICE AND ADDING TO LL */
+void newApprentice(int apprentice) {
+
+    if (!linkedList){
+        linkedList = newNode(apprentice);
+        
+        return;
+    }
+
+    struct Node *newAppr = newNode(apprentice);
+
+    newAppr->prev = linkedList;
+    newAppr->next = linkedList->next;
+    linkedList->next = newAppr;
+    linkedList = newAppr;
+
+    return;
+}
+
+
+void fireApprentice(int apprentice){
+
+
+}
+
 
 
 void *apprentice(void *j){
     int i = (int) j;
 
-    printf("Thread %d did something!\n",j);
+    while (1){
+        pthread_cond_wait(&cond, &mutex);
+    
+        flour -= 1;
+        oil -= 1;
+        bp -= 1;
+
+        breads += 1;
+        printf("Apprentice %d just made some bread.\n",i);
 
 
 
 
-    pthread_exit((void *)i);
+
+        if (breads == MAX_BREAD){
+            pthread_exit((void *)i);
+        }
+    }
+    
+    
 }
 
 
@@ -29,7 +105,7 @@ void *apprentice(void *j){
 
 int main() {
 
-    
+
 
     /* CREATING THREADS */
     pthread_t threads[N];
@@ -40,9 +116,15 @@ int main() {
             exit(1);
         } else {
             printf("Thread %d created!\n", i);
+            newApprentice(i);
+
+
         }
 
     }
+
+    /* LOGIC HANDLING ROUND ROBIN WITH LL */
+
 
 
 
@@ -57,9 +139,6 @@ int main() {
         printf("Thread is %d \n", val);
     }
     
-
-
-
 
 
     return 0;
