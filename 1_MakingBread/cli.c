75cf88fc299ec9e113491faab55f641c967dcd6e
Thirith Yang <thirith.yang@students.unibe.ch>
2022-05-20 10:37:29 +0200

    update

--- 1_MakingBread/cli.c
+++ 1_MakingBread/cli.c
@@ -12,85 +12,6 @@ void sleepingBaker();
 void testingScripts();
 
 
-/**
- * @brief Add one ingredient to our inventory, with the passed initialStock.
- * Either a new entry is created in our inventory, or the stock of an existing
- * entry is incremented by the passed initialStock values (for example, if
- * two bread-types use the same ingredient, we want to have only one entry in the
- * inventory).
- * @param name 
- * @param initialStock 
- */
-void addIngredient(const char* name, size_t initialStock)
-{
-    // search in our inventory, if we already have an entry for that ingredient
-    LinkedList* pList = getIngredient(name);
-    if(pList == NULL)
-    {
-        pList = CreateLinkedList(initialStock, name); // create a new entry in the inventory
-        // and add that new entry at the very end of the list:
-        for(int i = 0; i < inventoryLen; i++)
-        {
-            if(inventory[i] == NULL)
-            {
-                // we found an empty place, add it here
-                inventory[i] = pList;
-                break;
-            }
-        }
-    }
-    else
-    {
-        // just increment the stock of the already existing ingredient
-        while(initialStock > 0)
-        {
-            appendNode(pList);
-            initialStock--;
-        }
-    }
-}
-
-/**
- * @brief Just for fun, print the content of the inventory
- */
-void printInventory()
-{
-    printf("Inventory:\n");
-    for(int i = 0; i < inventoryLen; i++)
-    {
-        LinkedList* pList = inventory[i];
-        if(pList != NULL)
-        {
-            size_t count = getLength(pList);
-            printf("\tIngredient '%s' has %d items in stock\n", pList->name, count);
-        }
-    }
-}
-
-/**
- * @brief The struct defines a receipe for a bread-type
- */
-struct Receipe
-{
-    char* name; // the name of the bread-type
-    char* ingredients[3]; // the name of the 3 ingredients
-};
-typedef struct Receipe Receipe;
-Receipe* receipes;
-size_t receipesLen;
-
-/**
- * @brief Just for fun, print the receipes known
- */
-void printReceipes()
-{
-    printf("Receipes:\n");
-    for(int i = 0; i < receipesLen; i++)
-    {
-        Receipe r = receipes[i];
-        printf("\tReceipe '%s' needs those ingredients: '%s', '%s' and '%s'\n", r.name, r.ingredients[0], r.ingredients[1], r.ingredients[2]);
-    }
-}
 
 int main(/*int argc, char const *argv[]*/) {
 
@@ -159,35 +80,6 @@ void makingBread() {
     printf("\n\n");
 
 
-<<<<<<< HEAD
-    ////////////////////////////////
-    /* init linked list inventory */
-    ////////////////////////////////
-    // The max number of ingredients can be nr-of-bread-types * 3:
-    // Create an array to hold a linked-list for every ingredient.
-    // Init every place in the inventory with a null-pointer (not used place)
-    inventoryLen = nbBreadTypes * 3;
-    inventory = (LinkedList**) malloc(sizeof(LinkedList*) * inventoryLen);
-    memset(inventory, 0, sizeof(LinkedList*) * inventoryLen);
-
-    // Also init the array to store the receipes
-    receipesLen = nbBreadTypes;
-    receipes = malloc(sizeof(Receipe) * receipesLen);
-
-    //////////////////////////////////////////////////////////////
-    /* TODO: ADD CLI FOR AMOUNT FOR EACH INGREDIENT TO BE ADDED */
-    //////////////////////////////////////////////////////////////
-
-    /* ENTERING RECIPE FOR BREADS (three ingredients) */
-    printf("Now you are asked to enter three ingredients for each type of bread (max length %d). These ingredients may vary but do not have to.\n", maxStrLen);
-    for (int i = 0; i < nbBreadTypes; i++){     // For every bread type
-        // Create an entry for that receipe
-        Receipe r;
-        r.name = malloc(strlen(breadTypes[i]) * sizeof(char));
-        strcpy(r.name, breadTypes[i]);
-        for (int j = 0; j < 3; j++){            // there are three ingredients
-            printf("For bread type '%s', please enter ingredient #%d : ", &breadTypes[i], j+1);
-=======
     /* ENTERING AMOUNT OF DIFFERENT INGREDIENTS NECESSARY FOR EACH BREAD TYPE */
     printf("Next, please enter the different amount of ingredients each bread type's recipe requires.\n");
     for (int i = 0; i < nbBreadTypes; i++) {
@@ -213,37 +105,18 @@ void makingBread() {
     for (int i = 0; i < nbBreadTypes; i++) {     // For every bread type
         for (int j = 0; j < nbIngrArr[i]; j++) {            // there are ingredients
             printf("For bread type '%s', please enter ingredient #%d : ", breadTypesArr[i], j+1);
->>>>>>> d9687d001c478840582f73fd99f9597ab27078e7
             scanf("%s", inputString);               // Get ingredient number j for bread i from user
             if (strlen(inputString) > maxStrLen) {   // If the input for bread type name is greater than maxStrLen
                 printf("ERROR: Your String is too long! It should not be greater than %d.\n", maxStrLen);   
                 exit(1);                                                                                    
             }
-<<<<<<< HEAD
-            // ADD INGREDIENT TO INVENTORY (AND RECIPE)!!!
-            // add that ingredient, with an inital stock of 100
-            addIngredient(inputString, 100);
-            // and remember the ingredient name in the receipe
-            r.ingredients[j] = malloc(strlen(inputString) * sizeof(char));
-            strcpy(r.ingredients[j], inputString);
-
-            printf("Ingredient #%d : %s \n\n", j+1, inputString);
-=======
             strcpy(ingrNames[i][j], inputString);
             printf("Ingredient inserted #%d : %s \n", j+1, inputString);
->>>>>>> d9687d001c478840582f73fd99f9597ab27078e7
         }
-        receipes[i] = r;    // and finally store that receipe
     }
     free(inputString);      // freeing inputString
 
     printf("\n\n");
-<<<<<<< HEAD
-    
-    printInventory();
-    printReceipes();
-=======
->>>>>>> d9687d001c478840582f73fd99f9597ab27078e7
 
 
     printf("You also have to enter a desired amount of stock which is applied to each ingredient.\n");
